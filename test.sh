2dea73f5d68ba6ad99f795a22ddc004c095ec3f5
jfantou <fantou.jerome@gmail.com>
2023-04-30 20:57:03 +0900

    Update common yaml files

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 echo "Hi There! $1"
 git clone git@github.com:jfantou/$1.git
-cp /resources/yaml/test.yaml .~/$1
+cp /resources/yaml .~/$1
 git add .
 git commit -m "Update common yaml files"
 git push
